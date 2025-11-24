f016cd9f4608c7a30d8ac13fe5c71d5648d8a2a4
Tymo Verhaegen <r0878860@student.thomasmore.be>
2025-11-24 16:32:44 +0100

    small change

--- deployment/frontend/src/App.tsx
+++ deployment/frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom" // <--- 1. Import Navigate
 import NavBar from "./components/navbar"
 import HousePredictionModal from "@/components/ui/HousePredictionModal"
 
-// Páginas nuevas
 import HousingPage from "./pages/HousingPage"
 import ElectricityPage from "./pages/ElectricityPage"
 import InfoPage from "./pages/info"
@@ -14,10 +13,17 @@ function App() {
         <NavBar />
 
         <Routes>
-          <Route path="/" element={<HousePredictionModal />} />
+          {/* 2. Change the "/" route to use Navigate */}
+          <Route path="/" element={<Navigate to="/housing" replace />} />
+          
+          {/* 3. Keep your other routes */}
           <Route path="/housing" element={<HousingPage />} />
           <Route path="/electricity" element={<ElectricityPage />} />
           <Route path="/info" element={<InfoPage />} />
+          
+          {/* Optional: Add the modal as a separate route if you still need it, 
+              or render it inside HousingPage */}
+          {/* <Route path="/prediction" element={<HousePredictionModal />} /> */}
         </Routes>
       </div>
     </Router>
